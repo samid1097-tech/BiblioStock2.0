b1ab2ff98d588c6b839139aa1078011ebb48ab6f
davi662 <lopezmoli655@gmail.com>
2026-09-22 22:40:40 -0500

    feat(menu): agregar estructura inicial del menú de navegación de BiblioStock

---
+++ menu_interaccion/menu.py
@@ -0,0 +1,30 @@
+while True:
+    print("==========================")
+    print(" bienvenid@ a BiblioStock ")
+    print("==========================")
+    print(" ")
+    print("------- OPCIONES -------")
+    print("1. Registrar ítem")
+    print("2. Listar ítems")
+    print("3. Buscar ítem")
+    print("4. Registrar préstamo")
+    print("5. Registrar devolución")
+    print("6. Salir")
+
+    opcion=int(input(""))
+
+    if opcion == 1 :
+        pass
+    elif opcion == 2 :
+        pass
+    elif opcion == 3 :
+        pass
+    elif opcion == 4 :
+        pass
+    elif opcion == 5 :
+        pass
+    elif opcion == 6 :
+        print("Gracias por usar BiblioStock, regresa pronto.\nNo olvides regresar tus libros :)")
+        break
+    else:
+        print('OPCION INVALIDA, porfavor ingresa una opcion valida')
